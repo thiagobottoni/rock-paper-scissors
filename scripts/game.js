5292671f99d7c088d7f7f46a915e066e639e471c
Thiago Bottoni <bottonithiago@gmail.com>
2023-11-11 14:32:47 -0300

    First version of the rock paper scissors game

---
+++ scripts/game.js
@@ -0,0 +1,75 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+
+    switch (choice) {
+        case 1:
+            return "ROCK";
+            break;
+        case 2:
+            return "PAPER";
+            break;
+        case 3:
+            return "SCISSORS";
+            break;
+        default:
+            return "Error 001";
+    }
+}
+
+function play(player, computer) {
+    if (player === computer) {
+        return 2;
+    } else
+        if (
+            (player === "ROCK") && (computer === "SCISSORS") ||
+            (player === "SCISSORS") && (computer === "PAPER") ||
+            (player === "PAPER") && (computer === "ROCK")
+        ) {
+            return 1;
+        } else
+            if (
+                (computer === "ROCK") && (player === "SCISSORS") ||
+                (computer === "SCISSORS") && (player === "PAPER") ||
+                (computer === "PAPER") && (player === "ROCK")
+            ) {
+                return 0;
+            } else {
+                return -1;
+            }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    do {
+        let playerChoice = prompt("Whats your choice? Rock, paper or scissors?").toUpperCase();
+
+        let computerChoice = getComputerChoice();
+
+        let gameResult = play(playerChoice, computerChoice);
+
+        if (gameResult === 1) {
+            console.log("You win! " + playerChoice + " beats " + computerChoice);
+            playerScore++;
+        } else
+        if (gameResult === 0) {
+            console.log("You lose! " + computerChoice + " beats " + playerChoice);
+            computerScore++;
+        } else
+        if (gameResult === 2) {
+            console.log("It's a tie!");
+            playerScore++;
+            computerScore++;
+        } else {
+            console.log("Error 002");
+            break;
+        }
+    } while (
+        (playerScore < 5) && (computerScore < 5)
+    )
+
+    console.log("GAME RESULTS: \n PLAYER: " + playerScore + "\n COMPUTER: " + computerScore);
+}
+
+game();
